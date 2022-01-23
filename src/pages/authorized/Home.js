78d6840c3a544dd922c9ed40aae59423a715a908
Zed <emailzed03@gmail.com>
2022-01-23 13:15:39 +1100

    starting work on the home page

---
+++ src/pages/authorized/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Home extends React.Component {
+  
+  token = "";
+  
+  componentDidMount() {
+    // fetch token from cookie session
+    // query api for products
+    // populate ui with products
+  }
+
+  render() {
+    return (
+      <div>
+      home page 
+      </div>
+    )
+  }
+}
+
+export default Home;
