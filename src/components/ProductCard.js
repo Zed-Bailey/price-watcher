fb8e8cdf2f5a7242177fe6d4ca289771abe933f5
Zed <emailzed03@gmail.com>
2022-01-24 16:17:54 +1100

    fleshed out the main body of the card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,18 +3,29 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
 
 class ProductCard extends React.Component {
 
   render() {
+    const { ID, item_name, url, last_check, curr_price } = this.props.product;
+    
     return(
-      <Card variant='outlined' sx={{ minWidth: 275, marginBottom: 10}} key={this.props.ID}>
-        <CardContent>
-          <Typography variant='h5' gutterBottom>{this.props.item_name}</Typography>
-          <Typography color="text.secondary" component="div">last checked: {new Date(this.props.lastChecked).toUTCString()}</Typography>
-        </CardContent>
-      </Card>
+      <React.Fragment>
+        <Box sx={{minWidth: 300}}>
+          <Card variant='outlined'>
+            <CardContent>
+              <Typography variant='h5' gutterBottom align='left'>{item_name}</Typography>
+              <Typography variant='h5' gutterBottom align='left'>$ {curr_price}</Typography>
+              <Typography color="text.secondary" component="div" align='left'>last checked: {new Date(last_check).toUTCString()}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button>View</Button>
+            </CardActions>
+          </Card>
+        </Box>
+      </React.Fragment>
     )
   }
 }
