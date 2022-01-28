f7fc4084da9b6e9ec4c63309201187cddc7d97ac
Zed <emailzed03@gmail.com>
2022-01-28 12:22:25 +1100

    switched to a functional component, login error is working properly

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,59 +5,40 @@ import Container from '@mui/material/Container'
 import LoadingButton from '@mui/lab/LoadingButton'
 import Typography from '@mui/material/Typography'
 
-class Login extends React.Component {
+function Login() {
 
-  // navigation hook
-  // navigation = useNavigate();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [loginPressed, setLoginPressed] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
 
-  state = {
-    email: "",
-    password: "",
-    loginPressed: false,
-    errorMessage: ""
-  };
-
-  handleEmailInput = (event) => {
-    event.persist()
-    this.setState({
-      email: event.target.value,
-    })
+  const  handleEmailInput = (event) => {
+    event.persist();
+    setEmail(event.target.value);
   }
 
-  handlePasswordInput = (event) => {
-    event.persist()
-    this.setState({
-      password : event.target.value,
-    })
+  const  handlePasswordInput = (event) => {
+    event.persist();
+    setPassword(event.target.value);
   }
 
 
 
-  handleSuccessfulLogin(data) {
+  const handleSuccessfulLogin = () => {
     localStorage.setItem("isAuthenticated", true);
-    this.props.loginHandler(true);
     window.location.href = "/home";
   }
 
-   handleResponse(response) {
-     let data = response;
-     // if error occurred reject promise, this will then be handled by the catch block
-     if(!data.ok) {
-       console.log(data.json());
-      return Promise.reject(data.json().error);
-     } else {
-       this.handleSuccessfulLogin(data.json());
-     }
-   }
 
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     event.persist()
 
-    this.setState({loginPressed: true})
+    // this.setState({loginPressed: true})
+    setLoginPressed(true);
     // base64 encode the data
-    var joined = this.state.email + ':' + this.state.password
+    var joined = email + ':' + password
     var b64 = btoa(joined)
     // try to log user in
     fetch("http://localhost:8080/login", {
@@ -67,29 +48,36 @@ class Login extends React.Component {
       },
       credentials: 'include'
     })
-    .then(response => this.handleResponse(response))
-    .catch(error => {
-      this.setState({error : error, loginPressed: false});
+    .then(async (response) => {
+      if (response.ok) {
+        handleSuccessfulLogin();
+      } else {
+        setLoginPressed(false);
+        let data = await response.json()
+        console.log(data.error);
+        setErrorMessage(data.error);
+
+
+      }
     })
+   
   }
 
-  render() {
     // TODO fix error messages not showing above login button
-    return (
-      <Container maxWidth="sm" >
-        <form onSubmit={this.handleSubmit}> 
-          <Stack spacing={5}>
-            <TextField  label="email" variant="outlined" onChange={this.handleEmailInput} required type='email'/>
-            <TextField  label="password" variant="outlined" onChange={this.handlePasswordInput} type='password' required/>
-          </Stack>
-          <br/>
-          <Typography variant='caption' >{this.state.errorMessage}</Typography>
-
-          <LoadingButton loading={this.state.loginPressed} variant="outlined" type='submit'>Login</LoadingButton>
-        </form>
-      </Container>
-    );
-  }
+  return (
+    <Container maxWidth="sm" sx={{marginTop:5}}>
+      <form onSubmit={handleSubmit}> 
+        <Stack spacing={5}>
+          <TextField  label="email" variant="outlined" onChange={handleEmailInput} required type='email'/>
+          <TextField  label="password" variant="outlined" onChange={handlePasswordInput} type='password' required/>
+        </Stack>
+        <br/>
+        <Typography variant='body2' color='red' >{errorMessage}</Typography>
+        <br></br>
+        <LoadingButton loading={loginPressed} variant="outlined" type='submit'>Login</LoadingButton>
+      </form>
+    </Container>
+  );
 }
 
 export default Login;
