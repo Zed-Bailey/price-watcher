88b190504cc62005cc3c83cd87eb79f3baec384a
Zed <emailzed03@gmail.com>
2022-02-01 14:12:37 +1100

    updated the home state to only load products once on state change

--- src/pages/authorized/Home.js
+++ src/pages/authorized/Home.js
@@ -6,14 +6,12 @@ import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Add from '@mui/icons-material/Add';
 import AddProduct from '../../components/AddProduct';
-import CircularProgress from '@mui/material/CircularProgress';
-import Backdrop from '@mui/material/Backdrop';
 
 // TODO add loading notification
 export default function Home() {
     const [products, setProducts] = React.useState([]);
     const [open, setOpen] = React.useState(false);  
-    const [isLoading, setIsLoading] = React.useState(true)
+    // const [isLoading, setIsLoading] = React.useState(true)
     // this will be passed to the dialogs via prop, if an action is taken that
     // will update api, then set this to true and the screen should reload
     const [isChanged, setChanged] = React.useState(false);
@@ -31,6 +29,7 @@ export default function Home() {
       setChanged(true);
     }
   
+
     React.useEffect(() => {
       fetch('http://localhost:8080/private/items', {
         method: "GET",
@@ -38,19 +37,10 @@ export default function Home() {
       })
       .then(resp => resp.json())
       .then(json => {
-        setIsLoading(false);
         setProducts(json);
+        setChanged(false);
       })
-
-    }, [isLoading]);
-
-    React.useEffect(() => {
-      // this will trigger a reload via the useEffect watching it
-      setIsLoading(true);
-      setChanged(false);
     }, [isChanged]);
-
-    // populate ui with products
 
     return (
       <div>
@@ -58,8 +48,6 @@ export default function Home() {
           <Button sx={{float: 'right'}} variant="outlined" size='large' endIcon={<Add/>} onClick={handleOpen}>Add</Button>
         </Container>
         
-        {isLoading && <Backdrop open={isLoading}> <CircularProgress/> </Backdrop>}
-
         <Container sx={{marginTop:5}}>
           <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           {
